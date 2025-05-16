550d6a2204d34979681340fd0a869921bc77600f
zeptabot <brad_sparks@outlook.com>
2025-05-16 15:59:14 -0400

    Gradient boost
    Gradient Boost Algorithm to predict prices on all meaningful features on a 80%: 20% Train Test Split. Achieved 82.5% Prediction accuracy!

--- real-estate/python/average-price.py
+++ real-estate/python/average-price.py
@@ -133,7 +133,7 @@ X, y = load_and_prepare_data(json_url)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Train Random Forest Regressor
-rf = RandomForestRegressor(n_estimators=80, random_state=42)
+rf = RandomForestRegressor(n_estimators=10, random_state=42)
 rf.fit(X_train, y_train)
 
 # Predict on test set
